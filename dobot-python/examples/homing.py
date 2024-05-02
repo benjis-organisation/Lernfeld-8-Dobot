e9cd5ee814fcf02ffefd342877a2eb7958c64f28
Sebastian Jobst <sebastian_jobst@web.de>
2024-05-02 12:40:10 +0200

    Dobot Python initialize

---
+++ dobot-python/examples/homing.py
@@ -0,0 +1,13 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+
+from lib.interface import Interface
+
+bot = Interface('COM3')
+
+params = bot.get_homing_paramaters()
+print('Params:', params)
+
+print('Homing')
+bot.set_homing_command(0)
