1322ef7a7f0463ca8f5bb4ad3721ef2d46621ee2
Sebastian Jobst <sebastian_jobst@web.de>
2024-06-11 14:51:01 +0200

    Anpassung der automatischen Farberkennung

--- dobot-python/camera/Capture_an_image.py
+++ dobot-python/camera/Capture_an_image.py
@@ -1,41 +1,44 @@
 import cv2
 import time
 
-# Initialize the camera
-cap = cv2.VideoCapture(2)
+class FrameReadError(Exception):
+    pass
 
-# Create a window
-cv2.namedWindow('Open Camera', cv2.WINDOW_NORMAL)
+def initialize_camera():
+     # Initialize the camera
+    cap = cv2.VideoCapture(0)
+    return cap
 
-# Display the window for 10 seconds
-for _ in range(2):
-    # time.sleep(10)
+def capture_image(cap):
+    # Create a window
+    
+    cv2.namedWindow('Open Camera', cv2.WINDOW_NORMAL)
+
+    # Display the window for 10 seconds
+    for _ in range(2):
+        ret, frame = cap.read()
+        if not ret:
+            raise FrameReadError("Failed to read frame")
+        cv2.imshow('Open Camera', frame)
+        if cv2.waitKey(1000) & 0xFF == ord('q'):
+            break
+
+    
+    # Capture an image
     ret, frame = cap.read()
+
+    # Check if the image capture was successful
     if not ret:
-        print("Failed to read frame")
-        break
-    cv2.imshow('Open Camera', frame)
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
-        break
+        print("Failed to capture image")
+        exit(1)
 
-# Capture an image
-ret, frame = cap.read()
+    # Close the window
+    cv2.destroyWindow('Open Camera')
 
-# Check if the image capture was successful
-if not ret:
-    print("Failed to capture image")
-    exit(1)
+    # Save the image
+    cv2.imwrite('captured_image.jpg', frame)
 
-# Release the camera
-cap.release()
-
-# Close the window
-cv2.destroyWindow('Open Camera')
-
-# Save the image
-cv2.imwrite('captured_image.jpg', frame)
-
-# Display the image
-cv2.imshow('Image', frame)
-# cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Display the image
+    cv2.imshow('Image', frame)
+    # cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- dobot-python/steuerung/positioning.py
+++ dobot-python/steuerung/positioning.py
@@ -3,10 +3,11 @@ import os
 sys.path.insert(0, os.path.abspath('.')) # Erzeugt einen Pfad, der auf das aktuelle Verzeichnis zeigt
 
 from lib.dobot import Dobot
-# from camera.Calculate_color import calculate_color
-# from camera.Capture_an_image import capture_image
+from camera.Calculate_color import calculate_color
+from camera.Capture_an_image import *
 
 bot = Dobot('COM3')
+cap = initialize_camera()
 
 print('Bot status:', 'connected' if bot.connected() else 'not connected')
 
@@ -19,46 +20,62 @@ def move_and_suck(bot, positions, suck):
         bot.move_to(*position)
     bot.interface.set_end_effector_suction_cup(*suck)
 
-# color_positions = {
-#     "1": [(4.2, 67, 41.5, 0.5)],
-#     "2": [(-4, 65, 41.5, 0.5)],
-#     "3": [(4.4, 54, 55, 0.5)],
-#     "4": [(-4, 55, 55, 0.5)]
-# }
+color_positions = {
+    1: [(4.2, 67, 41.5, 0.5)],
+    2: [(-4, 65, 41.5, 0.5)],
+    3: [(4.4, 54, 55, 0.5)],
+    4: [(-4, 55, 55, 0.5)]
+}
 
-# color_movement = {
-#     "1": [(80, 60, 47.5, 0.5)],
-#     "2": [(60, 60, 47.5, 0.5)],
-#     "3": [(40, 60, 47.5, 0.5)],
-#     "4": [(20, 60, 47.5, 0.5)]
-# }
+color_movement = {
+    "red": [(80, 60, 47.5, 0.5)],
+    "green": [(60, 60, 47.5, 0.5)],
+    "blue": [(40, 60, 47.5, 0.5)],
+    "yellow": [(20, 60, 47.5, 0.5)]
+}
 
-# color_endpositions = {
-#     "red": [(80, 30, 30, 0.5)],
-#     "green": [(60, 30, 30, 0.5)],
-#     "blue": [(40, 30, 30, 0.5)],
-#     "yellow": [(20, 30, 30, 0.5)]
-# }
+color_endpositions = {
+    "red": [(80, 30, 30, 0.5)],
+    "green": [(60, 30, 30, 0.5)],
+    "blue": [(40, 30, 30, 0.5)],
+    "yellow": [(20, 30, 30, 0.5)]
+}
+try:
+    for i in range(1, len(color_positions) + 1):
+        print(f"Würfel Nummer {i}") #F wird für die Variable i eingesetzt
+        move_and_suck(bot, color_positions[i], (True, True))
+        print("Fahre zur Kamera")
+        move_and_suck(bot, [(-90, 50, 45, 0.5)], (True, True))
+        try:
+            capture_image(cap)     
+        except FrameReadError as e:
+            print("Bild konnte nicht aufgenommen werden")
+            move_and_suck(bot, [(-90, 50, 45, 0.5)], (False, False))
+            sys.exit(0) 
+        color_name = calculate_color()
+        if color_name in color_movement:
+            move_and_suck(bot, color_movement[color_name], (False, False))
+            move_and_suck(bot, color_endpositions[color_name], (False, False))
+        else:
+            print(f"Farbe unbekannt: {color_name}")
+            move_and_suck(bot, [(-90, 50, 45, 0.5)], (False, False))
+except Exception as e:
+    print(e)
+    move_and_suck(bot, [(-90, 50, 45, 0.5)], (False, False))
 
-# for i in range(1, len(color_positions) + 1):
-#     move_and_suck(bot, color_positions[i], (True, True))
-#     print("Fahre zur Kamera")
-#     move_and_suck(bot, [(-90, 50, 45, 0.5), color_movement[i]], (False, False))
-#     capture_image()
-#     print(f"Würfel Nummer {i}") #F wird für die Variable i eingesetzt
-#     color_name = calculate_color()
-#     if color_name in color_positions:
-#         bot.move_to(color_endpositions[color_name])
-#     else:
-#         print(f"Farbe unbekannt: {color_name}")
+# move_and_suck(bot, [(-90, 50, 45, 0.5)], (False, False))
 
-print("Würfel Nummer 1")
-move_and_suck(bot, [(4.2, 67, 41.5, 0.5)], (True, True))
+# print("Würfel Nummer 1")
+# move_and_suck(bot, [(4.2, 67, 41.5, 0.5)], (True, True))
 
-print("Fahre zur Kamera")
-move_and_suck(bot, [(-90, 50, 45, 0.5), (80, 60, 47.5, 0.5)], (False, False))
+# print("Fahre zur Kamera")
+# move_and_suck(bot, [(-90, 50, 45, 0.5)], (True, True))
 
-bot.move_to(80, 30, 30, 0.5)
+# capture_image()
+
+# move_and_suck(bot, [(80, 60, 47.5, 0.5)], (False, False))
+
+# bot.move_to(80, 30, 30, 0.5)
 
 # print("Würfel Nummer 2")
 # move_and_suck(bot, [(-4, 65, 41.5, 0.5)], (True, True))
